792d00b6af6cdd878751f65c61a49b1778441eaa
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2020-09-05 20:59:52 -0300

    Adicionado pacote intl no pubspec.yaml e formatando data com o pacote

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_despesas_pessoais/models/transaction.dart';
-
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -81,7 +81,7 @@ class MyHomePage extends StatelessWidget {
                           ),  
                         ),
                         Text(
-                          tr.date.toString(),
+                          DateFormat('d MMM y').format(tr.date),
                           style: TextStyle(
                             color : Colors.grey
                           ),
