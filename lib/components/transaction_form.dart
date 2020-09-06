ab0c4392463e654e7018d8d8b58bfdacea11049d
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2020-09-06 02:56:05 -0300

    transaction_form convertido pra stateful

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
+class TransactionForm extends StatefulWidget {
 
   final void Function(String, double) onSubmit;
 
   TransactionForm(this.onSubmit);
 
+  @override
+  _TransactionFormState createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
   final titleController = TextEditingController();
+
   final valueController = TextEditingController();
 
   _submitForm(){
     final title = titleController.text;
     final value = double.tryParse(valueController.text) ?? 0.0;
-    onSubmit(title, value);
+    widget.onSubmit(title, value);
 
     if(title.isEmpty || value <= 0){
       return;
