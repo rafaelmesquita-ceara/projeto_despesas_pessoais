b66cfbe0bf626f37f5bbd9c5ee32e973620594c2
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2020-09-06 03:04:56 -0300

    Modal fecha após transaction cadastrada

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,8 @@ class _MyHomePageState extends State<MyHomePage> {
       setState(() {
         _transactions.add(newTransaction);
       });
+
+      Navigator.of(context).pop();
   }
 
   final _transactions = [
