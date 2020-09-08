04ea929362a4fed8d1c3137ce2a5d37a743c7cc0
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2020-09-07 21:11:59 -0300

    Chart finalizado

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_despesas_pessoais/components/chart_bar.dart';
 import 'package:projeto_despesas_pessoais/models/transaction.dart';
 import 'package:intl/intl.dart';
 
@@ -36,14 +37,32 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get _weekTotalValue {
+    return groupedTransactions.fold(0.0, (previousValue, element) {
+      return previousValue + element['value'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    groupedTransactions;
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: [],
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactions.map((tr) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label : tr['day'],
+                value : tr['value'],
+                percentage: (tr['value'] as double) / _weekTotalValue,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
