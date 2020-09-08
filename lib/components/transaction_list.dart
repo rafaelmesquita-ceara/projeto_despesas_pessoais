dc384df02e050889338068eb1bbf651ed7cedfc5
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2020-09-07 21:44:10 -0300

    Usando ListTile para formar os blocos de transações na lista

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -4,7 +4,6 @@ import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
-
   final List<Transaction> transactions;
 
   TransactionList(this.transactions);
@@ -13,70 +12,53 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 500,
-      child: transactions.isEmpty ? Column(
-        children: [
-          SizedBox(height: 20),
-          Text(
-            'Nenhuma Transação Cadastrada!',
-            style: Theme.of(context).textTheme.title,
-          ),
-          SizedBox(height: 20),
-          Container(
-            height: 200,
-            child: Image.asset(
-              'assets/images/waiting.png',
-              fit : BoxFit.cover,
-            ),
-          ),
-        ],
-      ) : ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index){
-          final tr = transactions[index];
-          return Card(
-            child: Row(
+      child: transactions.isEmpty
+          ? Column(
               children: [
+                SizedBox(height: 20),
+                Text(
+                  'Nenhuma Transação Cadastrada!',
+                  style: Theme.of(context).textTheme.title,
+                ),
+                SizedBox(height: 20),
                 Container(
-                  margin : EdgeInsets.symmetric(
-                    horizontal : 15,
-                    vertical : 10
-                  ),
-                  decoration: BoxDecoration(
-                    border : Border.all(
-                      color : Theme.of(context).primaryColor,
-                      width : 2,
-                    )
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    'R\$ ${tr.value.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Theme.of(context).primaryColor,
-                    ),
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children : [
-                    Text(
-                      tr.title,
-                      style: Theme.of(context).textTheme.title, 
-                    ),
-                    Text(
-                      DateFormat('d MMM y').format(tr.date),
-                      style: TextStyle(
-                        color : Colors.grey
-                      ),
-                    )
-                  ]
-                )
               ],
-           ),
-          );
-        },
-      ),
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                final tr = transactions[index];
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(child: Text('R\$${tr.value}')),
+                      ),
+                    ),
+                    title: Text(
+                      tr.title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    subtitle: Text(
+                      DateFormat('d MMM y').format(tr.date),
+                    ),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
